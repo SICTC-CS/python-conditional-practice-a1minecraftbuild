75c4b27863f1e4c4dcb208e1a4f0f5a68d361090
a1minecraftbuild <146735686+a1minecraftbuild@users.noreply.github.com>
2023-11-16 20:15:45 -0600

    Add files via upload

---
+++ ThreeInts.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter your first number: "))
+number2 = int(input("Enter your second number: "))
+number3 = int(input("Enter your third number: "))
+numberList = [number1, number2, number3]
+
+numberList.sort()
+
+print(numberList)
